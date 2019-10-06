66da32ad8074b94c093409e2c85060e558abe171
sid <siddharthagoel1998@gmail.com>
2019-10-06 13:15:01 +0530

    ..

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3333
 
+app.get('/', (req, res)=>{
+	res.send("working !")
+})
+
 app.get('/download/:url', (req,res) => {
+	//http://localhost:4000/download/url?a=https://www.youtube.com/watch?v=42QuXLucH3Q
 	let URL = req.query.a;
 	res.header('Content-Disposition', 'attachment; filename="video.mp4"');
 	ytdl(URL, {
